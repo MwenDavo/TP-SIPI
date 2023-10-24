8485e86464cdba5e7992d6c3a8daa18836c8a1fd
Tomas Molina <molinatomas011@gmail.com>
2023-10-24 08:39:38 -0300

    Cambios Front, CORS añadido

---
+++ sipi/src/main/java/ar/edu/uade/sipi/configuracion/CorsConfig.java
@@ -0,0 +1,22 @@
+package ar.edu.uade.sipi.configuracion;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // Permitir solicitudes desde cualquier origen, puedes personalizar esto según tus necesidades.
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
